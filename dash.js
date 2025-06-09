794d653f9b9c2daecca61822705192780d6fc989
Maggie <maggiesun625@gmail.com>
2025-06-09 09:54:57 -0700

    fixed and added battery color

--- dash.js
+++ dash.js
@@ -6,24 +6,24 @@ window.addEventListener("DOMContentLoaded", ()=> {
         const batteryInfo = document.getElementById("batteryState");
         const batteryFill = document.getElementById("batteryFill");
 
-        batteryFill.style.width = `${percentage}%`;
-
         //assigns var to the element of id batteryState
         function updateBatteryDisplay(){
             percent = Math.round(battery.level * 100);
             const charging = battery.charging;
 
+            batteryFill.style.width = `${percent}%`;
+
             let text = "";
 
             if(charging){
                 text += "charging";
-                batteryFill.style.backgroundColor = "#00aaff";
+                batteryFill.style.backgroundColor = "#79A27D";
             } else if(percentage <= 20){
                 text += "low battery";
-                batteryFill.style.backgroundColor = "#ff4d4d";
+                batteryFill.style.backgroundColor = "#7a2e2e";
             } else{
                 text += "normal";
-                batteryFill.style.backgroundColor = "#4caf50"
+                batteryFill.style.backgroundColor = "#5C8A64";
             }
 
             percentage.textContent = `${percent}%`
